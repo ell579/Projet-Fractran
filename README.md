86728f267a4960f9ce2542fcdc89789c85e00aa7
ell579 <elliot.lieu@gmail.com>
2026-09-21 14:55:10 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Projet-Fractran
+Le main est diviser en 3 parties : le calcul des sommes de i+j tq 1<=i,j<=10, le calcul des produits de i*j pour ces mêmes i et j, le calcul de la suite de Fibonacci à partir de la fonction suite et des 1000 premiers nombres du programme et le calcul des premiers nombres premiers à partir la fonction suite et des 100000 premiers nombres du programme
+
+Explication du programme sur la suite de Fibonacci : on fait comme pour la somme et le produit en créant le programme (suite de fractions), puis on utilise la méthode suite en prenant comme terme initial 3 et on sort les 999 nombres générés par le programme, ensuite on regarde pour chaque nombre de la suite sortie et on ne garde que ceux ne possédant que 2 et 3 comme facteurs premiers, et on print la liste de ces derniers
+
---
+++ main.py
@@ -0,0 +1,55 @@
+from fractran import Fraction, Facteur, Fractran
+
+"""
+#Calcul des sommes
+
+somme = [Fraction(3, 2)]
+facteurs_somme = Facteur([2, 3, 5])
+
+for i in range(1, 11):
+    for j in range(1, 11):
+        resultat = Fractran(somme).run(facteurs_somme.nombre([i, j]))
+        somme_i_j = facteurs_somme.décomposition(resultat)
+        #print(f"{i} + {j} = {somme_i_j[1]}")
+
+
+#Calcul des produits
+
+produit = [Fraction(455, 33), Fraction(11, 13), Fraction(1, 11), Fraction(3, 7), Fraction(11, 2), Fraction(1, 3)]
+facteurs_produit = Facteur([2, 3, 5, 7, 11, 13])
+
+for i in range(1, 11):
+    for j in range(1, 11):
+        resultat = Fractran(produit).run(facteurs_produit.nombre([i, j, 0, 0, 0, 0]))
+        produit_i_j = facteurs_produit.décomposition(resultat)
+        print(f"{i} * {j} = {produit_i_j[2]}")
+"""
+
+#Suite de Fibonacci
+
+print("Fibonacci rend les couples (F(n), F(n+1)) :")
+fibonacci = [Fraction(23, 95), Fraction(57, 23), Fraction(17, 39), Fraction(130, 17), Fraction(11, 14),     #suite de fractions formant le programme Fractran
+          Fraction(35, 11), Fraction(19, 13), Fraction(1, 19), Fraction(35, 2), Fraction(13, 7), 
+          Fraction(7, 1)]
+
+sortie_brute = Fractran(fibonacci).suite(3, 1000)   #on calcule la suite de len 1000 de nombre généré par le programme en commençant par 3
+sortie = [] 
+for n in sortie_brute:
+    if n == Facteur([2, 3]).nombre(Facteur([2, 3]).décomposition(n)):
+        sortie.append(Facteur([2, 3]).décomposition(n))
+
+print(sortie)
+
+
+#Nombres premiers  
+
+print("Voici les nombres premiers générés par les 100000 premiers termes de la suite :")
+premiers = [Fraction(17, 91), Fraction(78, 85), Fraction(19, 51), Fraction(23, 38), Fraction(29, 33), Fraction(77, 29), Fraction(95, 23), Fraction(77, 19), Fraction(1, 17), Fraction(11, 13), Fraction(13, 11), Fraction(15, 14), Fraction(15, 2), Fraction(55, 1)]
+
+sortie_brute = Fractran(premiers).suite(2, 100000)
+sortie = []
+for n in sortie_brute:
+    if n == Facteur([2]).nombre(Facteur([2]).décomposition(n)):
+        sortie.append(Facteur([2]).décomposition(n)[0])
+
+print(sortie)
